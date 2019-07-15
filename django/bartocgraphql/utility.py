3425dd514711174932e88ce7a46b21fb147d466a
MHindermann <50413752+MHindermann@users.noreply.github.com>
2019-07-15 16:06:47 +0200

    Json to html (#27)
    * fix logic error w/ basic view
    
    * Update data.html
    
    * update basic view
    
    * add results.html
    
    * add parse function
    
    * add STANDARD_DUPLICATES
    
    * add type hints
    
    * fix results

--- django/bartocgraphql/utility.py
+++ django/bartocgraphql/utility.py
@@ -7,6 +7,7 @@ from openpyxl import load_workbook
 
 LOCAL_APP_PATH = "//itsc-pg2.storage.p.unibas.ch/ub-home$/hinder0000/Documents/GitHub/bartocgraphql/django/bartocgraphql/" # local app path
 STANDARD_TIME = 5 # standard search time in seconds
+STANDARD_DUPLICATES = False # standard value for duplicates argument
 
 class Database:
     """ A database """
--- django/bartocgraphql/views.py
+++ django/bartocgraphql/views.py
@@ -1,12 +1,16 @@
+""" views.py """
+
+from typing import List, Set, Dict, Tuple, Optional, Union
+
 import json
 import graphene
 
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from .forms import BasicForm, AdvancedForm  # local
 from .schema import Query                   # local
+from .utility import STANDARD_TIME, STANDARD_DUPLICATES # local
 
 QUERYSTRING = '''{
     resultsGlobal(SEARCHWORD, MAXSEARCHTIME, DUPLICATES) {
@@ -19,88 +23,125 @@ QUERYSTRING = '''{
   }
 }'''
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """ Landing page """
     
     context = {'landing_page': 'active'}
     return render(request, 'bartocgraphql/index.html', context)
 
-def about(request):
+def about(request: HttpRequest) -> HttpResponse:
     """ About page """
 
     context = {'about_page': 'active'}
     return render(request, 'bartocgraphql/about.html', context)
 
-def feedback(request):
+def feedback(request: HttpRequest) -> HttpResponse:
     """ About page """
 
     context = {'feedback_page': 'active'}
     return render(request, 'bartocgraphql/feedback.html', context)
-   
-def basic(request):
+
+def basic(request: HttpRequest) -> HttpResponse:
     """ Basic search for non-expert users """
 
     if request.method == 'GET':         
         form = BasicForm(request.GET)
         if form.is_valid():
             
-            query_string = QUERYSTRING
-            searchword = form.cleaned_data['searchword']
-            query_string = query_string.replace('SEARCHWORD', f'searchword: "{searchword}"')
-            query_string = query_string.replace(', MAXSEARCHTIME', '')
+            query_string = parse(form)
+
+            print(f'-->START') # dev
+            print(query_string) # dev
 
             schema = graphene.Schema(query=Query)
-            result = schema.execute(query_string) 
-            result_pretty = json.dumps(result.data, sort_keys=True, indent=4)
-            return HttpResponse(result_pretty,content_type='application/json')
+            result = schema.execute(query_string)
+
+            results = result.data.get('resultsGlobal') # we just need the values of 'resultsGlobal'
+            context = {'basic_page': 'active', 'results': results} 
+            return render(request, 'bartocgraphql/results.html', context)
     else:
         form = BasicForm()
     context = {'form': form, 'basic_page': 'active'}
     return render(request, 'bartocgraphql/basic.html', context)
 
-def data(request):
-    """ Data retrieval """
-
-    if request.method == 'GET':         
-        form = BasicForm(request.GET)   
-        if form.is_valid():
-            pass
-    else:
-        form = BasicForm()
-    context = {'form': form, 'data_page': 'active'}
-    return render(request, 'bartocgraphql/data.html', context)
-
-def advanced(request):
+def advanced(request: HttpRequest) -> HttpResponse:
     """ Advanced search """
 
     if request.method == 'GET':                             
         form = AdvancedForm(request.GET)        
         if form.is_valid():
 
-            query_string = QUERYSTRING # copy since we need the original
-            
-            searchword = form.cleaned_data['searchword'] # https://docs.djangoproject.com/en/2.2/ref/forms/api/#django.forms.Form.cleaned_data
-            query_string = query_string.replace('SEARCHWORD', f'searchword: "{searchword}"')                          
+            query_string = parse(form)
 
-            maxsearchtime = form.cleaned_data['maxsearchtime']
-            if maxsearchtime == None:
-                query_string = query_string.replace(', MAXSEARCHTIME', '')
-            else:
-                query_string = query_string.replace('MAXSEARCHTIME', f'maxsearchtime: {maxsearchtime}')
-
-            duplicates = form.cleaned_data['duplicates']
-            query_string = query_string.replace(', DUPLICATES', f', duplicates: {str(duplicates).lower()}')
             print(f'-->START') # dev
-            print(query_string)
+            print(query_string) # dev
 
-            # category = form.cleaned_data['category']
-                
             schema = graphene.Schema(query=Query)
-            result = schema.execute(query_string) # https://docs.graphene-python.org/en/latest/_modules/graphql/execution/base/#ExecutionResult
-            result_pretty = json.dumps(result.data, sort_keys=True, indent=4)
-            return HttpResponse(result_pretty, content_type='application/json')
+            result = schema.execute(query_string)
+
+            results = result.data.get('resultsGlobal') # we just need the values of 'resultsGlobal'
+            arguments = form.cleaned_data
+            context = {'advanced_page': 'active', 'results': results, 'arguments': arguments} 
+            return render(request, 'bartocgraphql/results.html', context)
     else:
         form = AdvancedForm()
     context = {'form': form, 'advanced_page': 'active'}
     return render(request, 'bartocgraphql/advanced.html', context)
+
+def data(request: HttpRequest) -> HttpResponse:
+    """ Data retrieval """
+
+    if request.method == 'GET':         
+        form = AdvancedForm(request.GET)   
+        if form.is_valid():
+
+            query_string = parse(form)
+            
+            print(f'-->START') # dev
+            print(query_string) # dev
+ 
+            schema = graphene.Schema(query=Query)
+            result = schema.execute(query_string)
+            result_pretty = json.dumps(result.data, sort_keys=True, indent=4)
+            return HttpResponse(result_pretty, content_type='application/json')
+    else:
+        form = AdvancedForm()
+    context = {'form': form, 'data_page': 'active'}
+    return render(request, 'bartocgraphql/data.html', context)
+
+def parse(form: Union[BasicForm, AdvancedForm]) -> str:
+    """ Update QUERYSTRING with arguments from form (if any) """
+
+    query_string = QUERYSTRING
+
+    # searchword:
+    searchword = form.cleaned_data['searchword'] 
+    query_string = query_string.replace('SEARCHWORD', f'searchword: "{searchword}"')                          
+
+    # maxsearchtime:
+    try:
+        maxsearchtime = form.cleaned_data['maxsearchtime']
+    except KeyError:
+        query_string = query_string.replace('MAXSEARCHTIME', f'maxsearchtime: {STANDARD_TIME}') # no form option
+    else:
+        if maxsearchtime == None:
+            query_string = query_string.replace('MAXSEARCHTIME', f'maxsearchtime: {STANDARD_TIME}') # form option off
+        else:
+            query_string = query_string.replace('MAXSEARCHTIME', f'maxsearchtime: {maxsearchtime}') # form option on
+
+    # duplicates:
+    try:
+        duplicates = form.cleaned_data['duplicates']
+    except KeyError:
+        query_string = query_string.replace(', DUPLICATES', f', duplicates: {str(STANDARD_DUPLICATES).lower()}') # no form option
+    else:
+        query_string = query_string.replace(', DUPLICATES', f', duplicates: {str(duplicates).lower()}') # form option on/off
+
+    # category:
+    # category = form.cleaned_data['category']
+
+    return query_string
+    
+
+
 
