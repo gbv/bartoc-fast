f4ead4e3d7139535f232f4f61aa3c0e1226ec2ab
MHindermann <50413752+MHindermann@users.noreply.github.com>
2019-07-16 15:59:15 +0200

    get_type, get_len

--- django/bartocgraphql/templatetags/filter.py
+++ django/bartocgraphql/templatetags/filter.py
@@ -1,8 +1,22 @@
+from typing import List, Set, Dict, Tuple, Optional, Union
+
 from django import template
+
+from ..models import SkosmosInstance, SparqlEndpoint
 
 register = template.Library()
 
 @register.filter
-def get(dictionary: dict, key: str):
-    """ Return the value for key """
-    return dictionary.get(key)
+def get_type(resource: Union[SkosmosInstance, SparqlEndpoint]) -> str:
+    """ Return the type of a resource"""
+    
+    if isinstance(resource, SkosmosInstance):
+        return "SKOSMOS"
+    if isinstance(resource, SparqlEndpoint):
+        return "SPARQL"
+
+@register.filter
+def get_len(thing: object) -> int:
+    """ Return len(thing) """
+    
+    return len(thing)
